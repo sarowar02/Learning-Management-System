81bb8f9994046dacfdfe448a10ccef96eed6e036
Sarowar <gsarowar639@gmail.com>
2024-11-22 20:00:08 +0600

    classroutine added in teacher controller

--- src/main/java/com/example/learningmanagementsystem/Controller/TeacherController.java
+++ src/main/java/com/example/learningmanagementsystem/Controller/TeacherController.java
@@ -1,16 +1,79 @@
 package com.example.learningmanagementsystem.Controller;
 
+import com.example.learningmanagementsystem.Model.ClassRoutine;
+import com.example.learningmanagementsystem.Model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.learningmanagementsystem.Repository.UserRepository;
+import com.example.learningmanagementsystem.Repository.ClassRoutineRepository;
+
+import java.util.List;
 
 @Controller
 class TeacherController {
 
 
+    @Autowired
+    private UserRepository UserRepository;
+
+    @Autowired
+    private ClassRoutineRepository ClassRoutineRepository;
     @GetMapping("/teacher")
-    private String teacherPage()
-    {
-        return "/teacher";
+    public String teacherPage(Model model, Authentication authentication) {
+        User teacher = UserRepository.findByEmail(authentication.getName());
+        List<ClassRoutine> routines = ClassRoutineRepository.findByTeacherEmail(teacher.getEmail());
+        model.addAttribute("routines", routines);
+        return "teacher";
+    }
+
+    @GetMapping("/teacher/classRoutine")
+    public String classRoutine(Model model, Authentication authentication) {
+        User teacher = UserRepository.findByEmail(authentication.getName());
+        List<ClassRoutine> routines = ClassRoutineRepository.findByTeacherEmail(teacher.getEmail());
+        model.addAttribute("routines", routines);
+        return "teacher";
+    }
+    @PostMapping("/teacher/add-routine")
+    public String addRoutine(@RequestParam String course_code,@RequestParam String subject , @RequestParam String room, @RequestParam String day,@RequestParam String semester,@RequestParam String time, Authentication authentication) {
+        User teacher = UserRepository.findByEmail(authentication.getName());
+        ClassRoutine routine = new ClassRoutine();
+        routine.settCourseCode(course_code);
+        routine.setSubject(subject);
+        routine.setRoom(room);
+        routine.setDay(day);
+        routine.setSemester(semester);
+        routine.setTime(time);
+        routine.setTeacherEmail(teacher.getEmail());
+        routine.setTeacherName(teacher.getFirstname()+" "+teacher.getLastname());
+        ClassRoutineRepository.save(routine);
+        return "redirect:/teacher/classRoutine";
+    }
+
+    @PostMapping("/teacher/classRoutine/update-routine")
+    public String updateRoutine(@RequestParam String course_code,@RequestParam String subject , @RequestParam String room, @RequestParam String day,@RequestParam String semester,@RequestParam String time) {
+        ClassRoutine routine = ClassRoutineRepository.findByCourseCode(course_code);
+        routine.setSubject(subject);
+        routine.setRoom(room);
+        routine.setDay(day);
+        routine.setSemester(semester);
+        routine.setTime(time);
+        ClassRoutineRepository.save(routine);
+        return "redirect:/teacher/classRoutine";
+    }
+
+
+
+    @GetMapping("/teacher/classRoutine/delete-routine")
+    @Transactional
+    public String deleteRoutine(@RequestParam String id) {
+        ClassRoutineRepository.deleteByCourseCode(id);
+        return "redirect:/teacher/classRoutine";
     }
 
 
---
+++ src/main/java/com/example/learningmanagementsystem/Repository/ClassRoutineRepository.java
@@ -0,0 +1,16 @@
+package com.example.learningmanagementsystem.Repository;
+
+import com.example.learningmanagementsystem.Model.ClassRoutine;
+import com.example.learningmanagementsystem.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClassRoutineRepository extends JpaRepository<ClassRoutine, String> {
+    List<ClassRoutine> findByTeacherEmail(String email);
+    ClassRoutine findByCourseCode(String courseCode);
+    void deleteByCourseCode(String courseCode);
+
+}
